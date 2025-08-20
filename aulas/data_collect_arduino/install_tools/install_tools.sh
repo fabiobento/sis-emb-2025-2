b6ee649f162d7e880ab97964207769a54a7869e1
fabiobento <fabio.obento@gmail.com>
2025-08-20 10:33:44 -0300

    inference

--- aulas/data_collect_arduino/install_tools/install_tools.sh
+++ aulas/data_collect_arduino/install_tools/install_tools.sh
@@ -74,10 +74,28 @@ sudo npm install -g edge-impulse-linux --force
 echo "Instalando dependências do PyAudio..."
 sudo apt-get install -y portaudio19-dev
 
-# Instalar o open-cv
+# Instala o OpenCV para Python
+echo "Instalando OpenCV para Python..."
 pip install opencv-python
 
-# Instala a biblioteca do Edge Impulse Linux SDK for Python
+# Instala a biblioteca do Edge Impulse Linux SDK for Python e PyAudio
+echo "Instalando Edge Impulse Linux SDK for Python e PyAudio..."
 pip install pyaudio edge_impulse_linux
+
+# Instala o pacote six
+echo "Instalando pacote six..."
+pip install six
+
+# Instala o pacote psutil
+echo "Instalando pacote psutil..."
+pip install psutil
+
+# Instala novamente portaudio19-dev (pode ser redundante)
+echo "Instalando novamente portaudio19-dev..."
+sudo apt-get install portaudio19-dev
+
+# Instala a biblioteca libstdc++6
+echo "Instalando libstdc++6..."
+sudo apt install libstdc++6
 
 echo "Instalação concluída!"
